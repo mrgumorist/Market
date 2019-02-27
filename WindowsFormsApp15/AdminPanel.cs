eeeb2cda250143db917c95d3089eb38918c7e408
Andriy <Andriy@DESKTOP-9LBFKE8>
2019-02-27 22:03:08 +0200

    апв

--- WindowsFormsApp15/AdminPanel.cs
+++ WindowsFormsApp15/AdminPanel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,25 @@ namespace WindowsFormsApp15
         public AdminPanel()
         {
             InitializeComponent();
+        }
+
+        private void AdminPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            Entitis.Context context = new Entitis.Context();
+            dataGridView1.DataSource = context.users.ToList();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Entitis.Context context = new Entitis.Context();
+            MessageBox.Show(context.users.Count().ToString());
         }
     }
 }
