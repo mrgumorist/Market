80faf7d693c515184615280e017dbd5c1d94b15a
Andriy <Andriy@DESKTOP-9LBFKE8>
2019-02-28 21:24:16 +0200

    павап

--- WindowsFormsApp15/AdminPanel.Designer.cs
+++ WindowsFormsApp15/AdminPanel.Designer.cs
@@ -49,7 +49,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(940, 175);
+            this.button1.Location = new System.Drawing.Point(940, 194);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(264, 43);
             this.button1.TabIndex = 1;
@@ -69,7 +69,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(940, 311);
+            this.button3.Location = new System.Drawing.Point(940, 347);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(264, 43);
             this.button3.TabIndex = 2;
@@ -79,7 +79,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(940, 442);
+            this.button4.Location = new System.Drawing.Point(940, 499);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(264, 43);
             this.button4.TabIndex = 3;
